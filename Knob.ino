7f514ec0bde17d35cbfdf0cc8ff76908f3a587bf
Minh <93916462+alexanderminhle@users.noreply.github.com>
2023-04-04 00:44:34 -0500

    Update Knob.ino

--- Knob.ino
+++ Knob.ino
@@ -1,12 +1,5 @@
-/*
- Controlling a servo position using a potentiometer (variable resistor)
- by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
-
- modified on 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Knob
-*/
-
+// Knob file from Servo Library to use definite values
+// Add button integration to this
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
