a5b5e8bb5d0a50a1773f1a54f54c311c085c4a23
Minh <93916462+alexanderminhle@users.noreply.github.com>
2023-04-20 13:36:56 -0500

    Update Knob.ino

--- Knob.ino
+++ Knob.ino
@@ -1,26 +1,84 @@
-/*
- Controlling a servo position using a potentiometer (variable resistor)
- by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
-
- modified on 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Knob
-*/
-
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+// constants won't change
+const int BUTTON_PIN1 = 7; // Arduino pin connected to button's pin
+const int BUTTON_PIN2 = 12;
+const int BUTTON_PIN3 = 2;
+const int SERVO_PIN1  = 9; // Arduino pin connected to first servo's pin
+const int SERVO_PIN2 = 8; // Arduino pin connected to second servo's pin
 
-int potpin = A0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
+// create servo objects to control the servos
+Servo servo1;
+Servo servo2;
+
+// variables will change:
+int angle = 0;          // the current angle of servo motors
+int lastButtonState1;    // the previous state of button
+int lastButtonState2;
+int lastButtonState3;
+int currentButtonState1; // the current state of button
+int currentButtonState2;
+int currentButtonState3;
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);                // initialize serial
+  pinMode(BUTTON_PIN1, INPUT_PULLUP); // set arduino pin to input pull-up mode
+  pinMode(BUTTON_PIN2, INPUT_PULLUP);
+  pinMode(BUTTON_PIN3, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+  servo1.attach(SERVO_PIN1);
+  servo2.attach(SERVO_PIN2);
+  servo1.write(angle);
+  servo2.write(angle);
+  currentButtonState1 = digitalRead(BUTTON_PIN1);
+  currentButtonState2 = digitalRead(BUTTON_PIN2);
+  currentButtonState3 = digitalRead(BUTTON_PIN3);
 }
 
 void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+  lastButtonState1    = currentButtonState1;      // save the last state
+  lastButtonState2    = currentButtonState2;
+  lastButtonState3    = currentButtonState3;
+  currentButtonState1 = digitalRead(BUTTON_PIN1);
+  currentButtonState2 = digitalRead(BUTTON_PIN2);
+  currentButtonState3 = digitalRead(BUTTON_PIN3);
+
+  if(lastButtonState1 == HIGH && currentButtonState1 == LOW) {
+    Serial.println("The button is pressed");
+
+    // change angle of servo motors
+    if(angle == 0)
+      angle += 90;
+    else
+    if(angle == 90)
+      angle = 0;
+
+    // control servo motors arccoding to the angle
+    servo1.write(angle);
+    servo2.write(angle);
+  }
+
+    if(lastButtonState2 == HIGH && currentButtonState2 == LOW) {
+    Serial.println("The button is pressed");
+
+    // change angle of servo motors
+    if(angle >= 0 && angle <= 90)
+      angle += 180;
+    else
+    if(angle >= 170)
+      angle = 0;
+
+    // control servo motors arccoding to the angle
+    servo1.write(angle);
+    servo2.write(angle);
+  }
+
+    if(lastButtonState3 == HIGH && currentButtonState3 == LOW) {
+    Serial.println("The button is pressed");
+
+      digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);    
+  }
 }
